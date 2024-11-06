f7c804600a9241b1516440fdd7bbb7f3418e54ae
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-06 18:15:20 +0100

    correzione esercizio

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-let listElements = document.getElementsByClassName("list-group-item");
-console.log(listElements);
-
 let listEmails = [];
-
-for (i = 1; i < 10; i++) {
+for (i = 1; i <= 10; i++) {
 	fetch("https://flynn.boolean.careers/exercises/api/random/mail")
 		.then((response) => response.json())
 		.then((data) => {
 			console.log(data.response);
 			listEmails.push(data.response);
-			console.log(listEmails);
+
+			if (listEmails.length == 10) {
+				let listElements = document.getElementsByClassName("list-group-item");
+				console.log(listElements);
+
+				for (i = 0; i < listElements.length; i++) {
+					listElements[i].innerText = listEmails[i];
+				}
+			}
 		});
-}
-for (i = 0; i < listElements.length; i++) {
-	listElements[i].innerText = listEmails[i];
+
+	console.log("chiamata", i);
+	console.log("elementi", listEmails.length);
 }
