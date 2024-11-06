dc6e6889eefab9abae1c3a92373125fbfcb9fe6e
AndreaBeltramin <andreabeltra99@gmail.com>
2024-11-06 16:54:48 +0100

    es sbagliato

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 let listElements = document.getElementsByClassName("list-group-item");
 console.log(listElements);
 
-for (i = 0; i < 10; i++) {
+let listEmails = [];
+
+for (i = 1; i < 10; i++) {
 	fetch("https://flynn.boolean.careers/exercises/api/random/mail")
 		.then((response) => response.json())
 		.then((data) => {
 			console.log(data.response);
-			for (i = 0; i < listElements.length; i++) {
-				listElements[i].textContent = data.response;
-			}
+			listEmails.push(data.response);
+			console.log(listEmails);
 		});
+}
+for (i = 0; i < listElements.length; i++) {
+	listElements[i].innerText = listEmails[i];
 }
